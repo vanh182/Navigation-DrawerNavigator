8da4032ccb8ccdc55006e389eeed2c19189dd63e
vanh182 <huynhvananh182@gmail.com>
2023-09-27 16:16:08 +0700

    add Bottom Tab

--- App.js
+++ App.js
@@ -2,13 +2,23 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import DrawerNavigator from "./navigation/DrawerNavigator";
+import BottomTabNavigator from "./navigation/TabNavigator";
 
 const App = () => {
     return (
       <NavigationContainer>
-        <DrawerNavigator />
+        <BottomTabNavigator/>
       </NavigationContainer>
     );
   }
   export default App;
+
+  // const App = () => {
+  //   return (
+  //     <NavigationContainer>
+  //       <DrawerNavigator/>
+  //     </NavigationContainer>
+  //   );
+  // }
+  // export default App;
 
